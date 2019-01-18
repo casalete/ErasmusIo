55aaef811f94373232b81b6016139ff10e16ceeb
monica <monica.mihai@esolutions.ro>
2019-01-18 22:57:07 +0200

    frrrrr

--- coursedialog.cpp
+++ coursedialog.cpp
@@ -1,5 +1,12 @@
 #include "coursedialog.h"
 #include "ui_coursedialog.h"
+#include "mainwindow.h"
+
+#include "QSql"
+#include "QSqlDatabase"
+#include <QDebug>
+#include <QSqlQuery>
+#include <QMessageBox>
 
 CourseDialog::CourseDialog(QWidget *parent) :
     QDialog(parent),
@@ -19,7 +26,40 @@ CourseDialog::~CourseDialog()
 
 void CourseDialog::on_buttonBox_accepted()
 {
-    accept();
+
+     accept();
+    //TODO - get values from ui
+
+    QString form_name = ui->course->text();
+    QString form_name_teacher = ui->teacher->text();
+     int form_no_hours = ui->nrH->value();
+      int form_no_credits = ui->nrCP->value();
+    QVariant form_optionality = ui->comboBox_optionality->currentText();
+
+    MainWindow m;
+    m.openConnection();
+    QSqlDatabase db = QSqlDatabase::database();
+    QSqlQuery query(db);
+    query.prepare("INSERT INTO course (name, name_teacher, no_credits, optionality, no_hours)"
+               "VALUES (?,?,?,?,?)");
+
+    query.addBindValue(form_name);
+    query.addBindValue(form_name_teacher);
+    query.addBindValue(form_no_hours);
+    query.addBindValue(form_no_credits);
+    query.addBindValue(form_optionality);
+
+    query.exec();
+
+    ui->course->clear();
+    ui->teacher->clear();
+    ui->nrH->clear();
+    ui->nrCP->clear();
+    ui->comboBox_optionality->clear();
+
+
+
+
 }
 
 void CourseDialog::on_buttonBox_rejected()
